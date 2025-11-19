77c890001ea6a8677acabc9002d4c6c215535062
mohdtameerkhan <khantameer087@gmail.com>
2025-11-19 12:34:22 +0300

    updation a video

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-
-
-
+import vdo from '../Assets/tvido.mp4';
 
 export default function Gallery() {
   return (
      <div className='max-w-[1100px] h-fit m-auto p-6 bg-amber-100'> 
      <h1 id='gallery' className='m-4 font-bold '>Turf_41 Highlights.
      </h1>
-     <div className='border p-2'>
-      
-     
+     <div className='border grid grid-cols-1 md:grid-cols-3 '>
+      <video src={vdo} autoPlay muted playsInline loop></video>
+      <div className='text-center '> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
+     <video src={vdo} autoPlay muted playsInline loop></video>
+     <div> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
+     <video src={vdo} autoPlay muted playsInline loop></video>
+     <div> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
+
       
       </div>
       
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -3,8 +3,11 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function Homepage() {
   return (
+    
     <div className=" max-w-[1100px] h-[70vh] flex flex-col justify-center m-auto text-amber-50 items-start p-4">
       {/* Typing Text */}
+      
+
       <h1 className="text-2xl md:text-1xl">
         <Typewriter
           words={["Book your turf anytime anywhere."]}
@@ -16,7 +19,6 @@ export default function Homepage() {
           delaySpeed={2000}
         />
       </h1>
-     
 
       {/* Button */}
       <a href='https://wa.me/917318454818' className="cursor-pointer mt-6 px-6 py-3 bg-green-600 text-white rounded-lg hover:bg-green-700 transition">
