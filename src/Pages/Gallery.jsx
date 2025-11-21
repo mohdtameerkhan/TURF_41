fcf396f677f9327cd2cfce742e9c79cf12b0df17
mohdtameerkhan <khantameer087@gmail.com>
2025-11-21 23:10:36 +0300

    updated vido

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import vdo from '../assets/tvido.mp4';
+import vdo2 from '../assets/IMG_4439.MP4';
 
 export default function Gallery() {
   return (
      <div className='max-w-[1100px] h-fit m-auto p-6 bg-amber-100'> 
      <h1 id='gallery' className='m-4 font-bold '>Turf_41 Highlights.
      </h1>
-     <div className='border grid grid-cols-1 md:grid-cols-3 '>
+     <div className='border grid grid-cols-1 md:grid-cols-3 gap-4 p-4'>
+       
       <video src={vdo} autoPlay muted playsInline loop></video>
       <div className='text-center '> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
-     <video src={vdo} autoPlay muted playsInline loop></video>
+     <video src={vdo2} autoPlay muted playsInline loop></video>
      <div> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
      <video src={vdo} autoPlay muted playsInline loop></video>
      <div> repellat consequatur aperiam laudantium iste nam hic iure ex minus assumenda commodi doloribus ad quis? Illum, aperiam! </div>
